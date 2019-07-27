34f192fb285cbb07fdcdb1ace281cccb1e856d2c
syai <nws06_01087923@pb.edu.bn>
2019-07-27 13:42:12 +0800

    syai testing

---
+++ bytheway/src/login-page/login-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splashscreen-page',
+  templateUrl: './splashscreen-page.page.html',
+  styleUrls: ['./splashscreen-page.page.scss'],
+})
+export class SplashscreenPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
